d15bf785cfc14b643d4764b7fed52fd661848e59
Dokyun Lim <im.koremp@gmail.com>
2022-12-13 21:37:05 +0900

    refact: move router to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Home from './pages/Home';
-import Error from './pages/Error';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <></>,
-    errorElement: <Error />
-  },
-  {
-    path: "/home",
-    element: <Home />
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />
-  },
-  {
-    path: "/sign-in",
-    element: <SignIn />
-  },
-])
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
