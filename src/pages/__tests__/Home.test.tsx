0be69964a8e867b3fcaafa408fb6033b94ba4131
Dokyun Lim <im.koremp@gmail.com>
2022-12-13 23:29:41 +0900

    test: add checking title and buttons to homepage test

--- src/pages/__tests__/Home.test.tsx
+++ src/pages/__tests__/Home.test.tsx
@@ -2,9 +2,21 @@ import { render } from '@testing-library/react';
 import Home from '../Home';
 
 describe('Home', () => {
-  it('renders text.', () => {
+  it('renders Title and Description.', () => {
     const { getByText } = render(<Home />);
 
-    expect(getByText('Home')).not.toBeNull();
+    expect(getByText('만다라트')).not.toBeNull();
+    expect(getByText('나만의 목표를 만들어요')).not.toBeNull();
+  })
+
+  it('renders Login Buttons', () => {
+    const { getByRole } = render(<Home />);
+
+    expect(getByRole('button', {
+      name: "Google"
+    })).not.toBeNull();
+    expect(getByRole('button', {
+      name: "Kakao"
+    })).not.toBeNull();
   })
 })
