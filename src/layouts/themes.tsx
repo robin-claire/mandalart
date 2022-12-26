f12bfa14f927fdc142cc6aa59c2d3c1172e4858f
Dokyun Lim <im.koremp@gmail.com>
2022-12-26 22:53:51 +0900

    feat: add basic layout themes

---
+++ src/layouts/themes.tsx
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/system';
+
+export const theme = createTheme{
+  button: {
+    primary: '', //BLUE
+  },
+  default: {
+    backgroundColor: '', // DEEP GREY
+  },
+  goalpage: {
+    coreGoalBackgroundColor: '', // MIDDLE GREY
+    detailGoalBackgroundColor: '', // GREY
+    backgroundColor: '', // GREY
+  },
+  colors: {
+    red: '',
+    orange: '',
+    yellow: '',
+    green: '',
+    blue: '',
+    purple: '',
+    black: '',
+  }
+}
