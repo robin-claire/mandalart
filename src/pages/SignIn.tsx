eee8c6b00c1d19331a7b0c4fc30e090f95cbf1ec
Dokyun Lim <im.koremp@gmail.com>
2022-12-30 15:17:50 +0900

    reefact: update signin constants

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,39 +1,48 @@
 import {
   Grid,
-  Box,
   Typography,
-  TextField,
-  FormControlLabel,
-  Checkbox,
+  Box,
   Button,
 } from '@mui/material';
 
+import { SignInPage } from '../fixtures/constants';
+//import { SignInPage } from 'fixtures/constants';
+
 // import PageBox from 'components/PageBox';
 import PageBox from '../components/PageBox';
-// import DescButton from 'components/agreement/DescButon';
-import DescButton from '../components/agreement/DescButon';
 
 function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const { platform, text } = SignInPage.buttons;
 
   return (
     <PageBox>
       <Grid container>
         <Grid item xs={12} mt={8}>
           <Typography variant='h2'>
-            만다라트
+            {SignInPage.title}
           </Typography>
+          <Typography variant='h4'>
+            {SignInPage.description.first}
+          </Typography>
+          <Typography variant='h4'>
+            {SignInPage.description.second}
+          </Typography>
+          <Box
+            sx={{
+              p: 4,
+            }}>
+            {
+              platform.map(({ title, color, icon }) => (
+                <Button sx={{ mt: 2, backgroundColor: color.container }} fullWidth key={title} variant="contained" startIcon={icon}>
+                  {title}{text}
+                </Button>
+              ))
+            }
+          </Box>
         </Grid>
       </Grid>
     </PageBox>
-  )
+  );
 }
 
 export default SignIn;
