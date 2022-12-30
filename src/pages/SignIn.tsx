8f0ffd6487e6e1baea419237ab46654f4b5d6f28
Dokyun Lim <im.koremp@gmail.com>
2022-12-30 14:47:02 +0900

    refact: remove items in signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -8,7 +8,10 @@ import {
   Button,
 } from '@mui/material';
 
-import { PageBox } from '../components/PageBox';
+// import PageBox from 'components/PageBox';
+import PageBox from '../components/PageBox';
+// import DescButton from 'components/agreement/DescButon';
+import DescButton from '../components/agreement/DescButon';
 
 function SignIn() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -20,54 +23,13 @@ function SignIn() {
     });
   };
 
-
   return (
     <PageBox>
       <Grid container>
         <Grid item xs={12} mt={8}>
           <Typography variant='h2'>
-          Sign In Page
+            만다라트
           </Typography>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{ mt: 1, p: 4 }}
-        >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            aria-label='Email Address'
-            name="email"
-            autoComplete="email"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            aria-label='Password'
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-        </Box>        
         </Grid>
       </Grid>
     </PageBox>
