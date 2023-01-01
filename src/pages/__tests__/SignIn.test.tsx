bf2ccc1765bc7562858803e69f3367659a4257b6
Dokyun Lim <im.koremp@gmail.com>
2023-01-01 11:38:57 +0900

    test: change sign in page test to use constants

--- src/pages/__tests__/SignIn.test.tsx
+++ src/pages/__tests__/SignIn.test.tsx
@@ -1,39 +1,33 @@
 import { render } from '@testing-library/react';
 import SignIn from '../SignIn';
 
+import { SignInPage } from 'fixtures/constants';
+
 describe('SignIn', () => {
   it('renders title.', () => {
     const { getByText } = render(<SignIn />);
 
-    expect(getByText('Sign In Page')).not.toBeNull();
+    expect(getByText(SignInPage.title)).not.toBeNull();
+  });
+
+  it('renders description.', () => {
+    const { getByText } = render(<SignIn />);
+    {
+      SignInPage.description.forEach((text) => {
+        expect(getByText(text)).not.toBeNull();
+      })
+    }
   })
 
-  it('renders e-mail and password inputs.', () => {
-    const { getByLabelText } = render(<SignIn />);
-
-    expect(getByLabelText('Email Address'));
-    expect(getByLabelText('Password'));
+  it('renders mandalart logo.', () => {
+    //const { getByRole } = render(<SignIn />);
   })
 
-  it('renders Sign-In button.', () => {
-    const { getByRole } = render(<SignIn />);
-
-    expect(getByRole('button', {
-      name: "Sign In",
-    })).not.toBeNull();
+  it('renders sign in buttons.', () => {
+    //const { getByRole } = render(<SignIn />);
   })
 
-  describe('when sign in button clicks,', () => {
-    it('when inputs are not valid, shows error.', () => {
-      const { getByText } = render(<SignIn />);
-
-      expect(getByText('Sign In')).not.toBeNull();
-    })
-
-    it('when inputs are valid, occurs handleSubmit', () => {
-      const { getByText } = render(<SignIn />);
-
-      expect(getByText('Sign In')).not.toBeNull();
-    })
+  it('when platform sign in button is clicked, occurs handleClick.', () => {
+    //const { getByRole } = render(<SignIn />);
   })
-})
+});
